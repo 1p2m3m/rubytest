242f50d667ace7d65b11b7e61713ea56f3d84944
1p2m3m <pmezger7@gmail.com>
2018-02-03 16:07:42 +0100

    add first program to respository

---
+++ code1/helloworld.rb
@@ -0,0 +1,9 @@
+
+puts "Pleaae enter your name"
+name = gets.chomp
+puts "Please enter your fav color"
+color = gets.chomp
+puts "Please enter you age"
+age = gets.chomp
+
+puts "Hello my name is #{name}. \nMy fav color is #{color} and I am #{age} years old"
